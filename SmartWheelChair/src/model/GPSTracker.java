aa257c2b40679fae206a51dfbca590325dd3c8ed
Toumie <64904807+Toumie@users.noreply.github.com>
2021-05-31 21:01:51 +0200

    Update GPSTracker.java
    gps tracker

--- SmartWheelChair/src/model/GPSTracker.java
+++ SmartWheelChair/src/model/GPSTracker.java
@@ -7,8 +7,44 @@ package model;
 
 /**
  *
- * @author meriam
+ * @author TOUMIE
  */
 public class GPSTracker {
     
+    private String currentLocation;
+    private String Destination;
+
+    public GPSTracker() {
+    }
+
+    public GPSTracker(String currentLocation, String Destination) {
+        this.currentLocation = currentLocation;
+        this.Destination = Destination;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public String getDestination() {
+        return Destination;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public void setDestination(String Destination) {
+        this.Destination = Destination;
+    }
+    
+    public double calculateDistance(String currentLocation, String Destination)
+    {
+    return 0;
+    }
+    
+    public String changeDirection()
+    {
+    return ("");
+    }
 }
