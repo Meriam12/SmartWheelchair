efe22bdeba6077ca40cd3a4be46976c8f6cd7286
Meriam Sherif <meriam_kamel@yahoo.com>
2021-06-05 21:41:03 +0200

    meriam change

--- SmartWheelChair/src/view/GUI.java
+++ SmartWheelChair/src/view/GUI.java
@@ -695,7 +695,7 @@ public class GUI extends javax.swing.JFrame {
                         .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addGap(37, 37, 37)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                //.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -709,7 +709,7 @@ public class GUI extends javax.swing.JFrame {
                         .addComponent(HandBrake, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(59, 59, 59)
                         .addComponent(jRadioButton4)
-                        .addGap(32, 32, 32))))
+                       // .addGap(32, 32, 32))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -857,6 +857,7 @@ public JRadioButton getchangelocation() {
         public JRadioButton getBackRadio() {
         return BackRadio;
     }
+        
 
 //    public JTextField getjTextField1() {
 //        return locbtn;
