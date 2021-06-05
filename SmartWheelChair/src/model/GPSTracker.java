1179bb6a8dac5028e942a8acea5232f6614e9577
ShroukkNabil <shrouk.nabil239@gmail.com>
2021-06-05 12:12:11 +0200

    gps
    gps

--- SmartWheelChair/src/model/GPSTracker.java
+++ SmartWheelChair/src/model/GPSTracker.java
@@ -20,10 +20,13 @@ public class GPSTracker extends Thread {
      private GUI gui;
     private String currentLocation;
     private String Destination;
-
+private Wheelchair wheelchair;
     public GPSTracker() {
     }
-
+ public GPSTracker(Wheelchair wheelchair) {
+        this.wheelchair = wheelchair;
+    }
+    
     public GPSTracker(String currentLocation, String Destination) {
         gui = new GUI();
         this.currentLocation = currentLocation;
@@ -31,12 +34,8 @@ public class GPSTracker extends Thread {
     }
 
     public String getCurrentLocation() {
-        currentLocation = gui.getjTextField1().getText();
-         if (currentLocation=="cairo")
-         {
-             String res= currentLocation= one;
-         } else {
-         String res= currentLocation= two;}
+        //this.currentLocation = gui.getjTextField1().getText();
+   
         return currentLocation;
     }
 
@@ -46,20 +45,23 @@ public class GPSTracker extends Thread {
 
     public void setCurrentLocation(String currentLocation) {
        
-      //  this.currentLocation = currentLocation;
-         this.currentLocation = gui.getjTextField1().getText();
-         
+       this.currentLocation = currentLocation;
+        
+   
+       
     }
 
     public void setDestination(String Destination) {
         this.Destination = Destination;
     }
     
-    public double calculateDistance(String currentLocation, String Destination)
-    {
-    return 0;
-    }
-    
+//    public double calculateDistance(String currentLocation, String Destination)
+//    {
+//         this.currentLocation = gui.getjTextField1().getText();
+//          this.currentLocation = gui.getjTextField1().getText();
+//    return 0;
+//    }
+//    
     public String changeDirection()
     {
     return ("");
